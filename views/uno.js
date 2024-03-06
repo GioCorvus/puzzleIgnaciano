eb1df03e62d1aa225e8265fb41932395a9234dd1
Mario Fuentes <mariofuentesf16.gmail.com>
2024-03-06 10:05:14 +0100

    script crear grid

--- views/uno.js
+++ views/uno.js
@@ -6,10 +6,27 @@ export class Uno extends Vista {
   constructor(controlador, base) {
     super(controlador, base);
     this.restService = new Rest();
+    this.crearGrid(2,2)
   }
 
   crearGrid(x,y){
+    //X Columnas
+    //Y Filas
+    console.log("CrearGrid")
+    const tablero = document.getElementById('puzzle');
+    tablero.innerHTML = '';
 
+    tablero.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+    tablero.style.gridTemplateRows = `repeat(${y}, 1fr)`;
+
+    for (let i = 0; i < x * y; i++) {
+        const celda = document.createElement('div');
+        celda.className="celda";
+        tablero.appendChild(celda);
+    }
   }
 
+  validarPuzzle(){
+
+  }
 }
